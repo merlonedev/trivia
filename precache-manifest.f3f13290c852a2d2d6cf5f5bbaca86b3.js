31a305502e86379ebb6e4204b9eefaaf8922b1b5
merlone <alestori7@gmail.com>
2021-10-03 11:05:20 -0300

    Updates

---
+++ precache-manifest.f3f13290c852a2d2d6cf5f5bbaca86b3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db04d423af9edd7479aa4200b66efcd",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "783e32993ab179cb51dd",
+    "url": "/trivia/static/css/main.2c00424c.chunk.css"
+  },
+  {
+    "revision": "f19f7edde4443145efed",
+    "url": "/trivia/static/js/2.120ad2c4.chunk.js"
+  },
+  {
+    "revision": "be38966b493df8d545bcf6c007c77b34",
+    "url": "/trivia/static/js/2.120ad2c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "783e32993ab179cb51dd",
+    "url": "/trivia/static/js/main.ecbfa95a.chunk.js"
+  },
+  {
+    "revision": "23dbb679c736924e4b1a",
+    "url": "/trivia/static/js/runtime-main.6e763e45.js"
+  },
+  {
+    "revision": "74d647e8978495e794a0fb8a245185b7",
+    "url": "/trivia/static/media/Earth-Spinning-Stars.74d647e8.jpg"
+  },
+  {
+    "revision": "648788a9e52954ff17e54e488a95ee7a",
+    "url": "/trivia/static/media/EternalLogo-MVO7Y.648788a9.ttf"
+  },
+  {
+    "revision": "335114be24fd194b6a561f94de40263e",
+    "url": "/trivia/static/media/interstellar.335114be.png"
+  },
+  {
+    "revision": "9b5f279bd9fa7937aa3372db8ac99aca",
+    "url": "/trivia/static/media/settings.9b5f279b.png"
+  }
+]);
